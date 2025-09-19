96c67067d52bf179d3f5cdce3cdc12dbd59659c1
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-09-19 19:45:42 +0500

    Added Lab 3 Q4

---
+++ DSA Lab 3/Q4.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Name : Muhammad Umar
+    Class : CSIT Section A Batch 24
+    Roll no. : CT - 24028
+*/
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int data) {
+        this->data = data;
+        next = prev = nullptr;
+    }
+};
+
+class Doubly_Linked_List {
+public:
+    Node* head;
+    Node* tail;
+    Doubly_Linked_List() {
+        head = tail = nullptr;
+    }
+
+    bool isEmpty() {
+        return head == nullptr;
+    }
+
+    void push_back(int val) {
+        Node* n = new Node(val);
+        if (isEmpty()) {
+            head = tail = n;
+            return;
+        }
+        n->prev = tail;
+        tail->next = n;
+        tail = n;
+    }
+    
+
+    void insertSorted(int value) {
+        Node* n = new Node(value);
+        
+        if (head == nullptr) {
+            head = tail = n;
+            return;
+        }
+
+        if (value < head->data) {
+            n->next = head;
+            head->prev = n;
+            head = n;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next != nullptr && value > current->next->data) {
+            current = current->next;
+        }
+        
+        
+        n->next = current->next;
+        n->prev = current;
+        
+    	if(current->next != nullptr){
+    		n->next->prev = n;
+	}else{
+		tail=n;
+	}
+        
+        
+        current->next = n;
+
+    }
+
+
+    void sort() {
+        Doubly_Linked_List sorted;
+        Node* current = head;
+        
+        while (current != nullptr) {
+            sorted.insertSorted(current->data);
+            current = current->next;
+        }
+        this->head = sorted.head;
+        this->tail = sorted.tail;
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " <-> ";
+            temp = temp->next;
+        }
+        cout << "nullptr\n\n";
+    }
+
+};
+
+
+Doubly_Linked_List concatenate(Doubly_Linked_List L, Doubly_Linked_List M) {
+    Doubly_Linked_List N;
+
+    Node* temp = L.head;
+    while (temp != nullptr) {
+        N.push_back(temp->data);
+        temp = temp->next;
+    }
+
+    temp = M.head;
+    while (temp != nullptr) {
+        N.push_back(temp->data);
+        temp = temp->next;
+    }
+
+    return N;
+}
+
+int main() {
+    Doubly_Linked_List L; // Doubly Linked List for Even Numbers
+    Doubly_Linked_List M; // Doubly Linked List for Odd Numbers
+
+    for (int i = 2; i <= 10; i += 2) L.push_back(i);
+    cout << "List L : \n";
+    L.print();
+
+    for (int i = 1; i <= 9; i += 2) M.push_back(i);
+    cout << "List M : \n";
+    M.print();
+
+    Doubly_Linked_List N = concatenate(L, M);
+    cout << "List N (concatenation of L and M): \n";
+    N.print();
+
+    N.sort();
+    cout << "Sorted List N : \n";
+    N.print();
+
+    return 0;
+}
